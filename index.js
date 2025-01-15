a6d46f9274c6bfa655f3e3a9918f1a7cb663b31c
Diego Sanchez-Carapia <diegolinsanchez4@gmail.com>
2025-01-14 20:13:12 -0500

    OnlyFootball Update

--- index.js
+++ index.js
@@ -1,4 +1,4 @@
-async function fetchData() {
+async function fetchData(leagueVal) {
     const options = {
         method: 'GET',
         headers: {
@@ -7,24 +7,94 @@ async function fetchData() {
         }
     };
 
-    const response = await fetch('https://api-football-v1.p.rapidapi.com/v3/standings?league=39&season=2024',options);
+    const response = await fetch(`https://api-football-v1.p.rapidapi.com/v3/standings?league=${leagueVal}&season=2024`,options);
     const data = await response.json();
-    //console.log(data.response[0].team.name); //getting a club name
     return data.response;
 }
 
+async function fetchTeam(clubName) {
+    const options = {
+        method: 'GET',
+        headers: {
+            'x-rapidapi-key': 'd529d55796msh6121da346c2f555p174182jsn49c74ade16ed',
+            'x-rapidapi-host': 'api-football-v1.p.rapidapi.com'
+        }
+    };
 
+    const response = await fetch(`https://api-football-v1.p.rapidapi.com/v3/teams?name=${clubName.replace(/ /g, "%20")}`,options);
+    const data = await response.json();
+    return data.response[0];
+}
+
+async function renderClubsByName(event) {
+
+    const clubList = document.body.querySelector(".results__list");
+    clubList.innerHTML = `<i class="fa-solid fa-spinner spinner"></i>`;
+    
+    if(event.keyCode == 13){
+        let value = document.querySelector(".nav__input").value;
+        let data  = await fetchTeam(value);
+        clubHTML(data);
+    }
+    
+    
+}
 
 async function renderClubs() {
+    let leagueVal = document.body.querySelector('#team-select').value;
+
     const clubList = document.body.querySelector(".results__list");
-    //clubList.innerHTML = `<i class="fa-solid fa-spinner spinner"></i>`;
+    clubList.innerHTML = `<i class="fa-solid fa-spinner spinner"></i>`;
 
-    const leagueVal = document.body.querySelector('#team-select').value;
-
-    let data = await fetchData();
-
+    let data = await fetchData(leagueVal);
     const league = data[0].league.name
     data = data[0].league.standings[0];
+
+    if(document.body.querySelector('.options__view').value === "false"){
+        console.log("here");
+        data = data.slice(0,6);
+    }
+    
+    clubsHTML(data,league);
+    
+}
+
+const clubHTML = (data) =>{
+    const searchResult = document.querySelector(".searchInputHere");
+    searchResult.innerHTML = `<h1>${data.team.name}</h1>`;
+    const clubList = document.body.querySelector(".results__list");
+    clubList.innerHTML = `<i class="fa-solid fa-spinner spinner"></i>`;
+
+    document.body.querySelector('#team-select').value = "";
+
+    clubList.innerHTML = `<div class="result">
+                            <figure class="result__image__wrapper">
+                                <img class="result__image" src="${data.team.logo}" alt="">
+                            </figure>
+                            <div class="result__info">
+                                <div class="result__info__name">${data.team.name}</div>
+                                <div class="result__info__league">${data.team.code}</div>
+                            </div>
+                            <div class="result__info__hover">
+                                <div class="result__info__hover__stats">
+                                    <div class="result__info__hover__league-points">${data.team.code}</div>
+                                    <div class="result__info__hover__stat">Venue: ${data.venue.name}</div>
+                                    <div class="result__info__hover__stat">Capacity: ${data.venue.capacity}</div>
+                                </div>
+                                <div class="result__info__hover__game">Founded: <span class="nextgame">${data.team.founded}</span></div>
+                            </div>
+                            <div class="result__info__hover__country">${data.team.country}</div>
+                        </div>`;
+
+    
+}
+
+const clubsHTML = (data, league) =>{
+    
+    const clubList = document.body.querySelector(".results__list");
+
+    const searchResult = document.querySelector(".searchInputHere");
+    searchResult.innerHTML = `<h1></h1>`;
 
     const clubsHTML = data.map((data) => {
         return `<div class="result">
@@ -46,12 +116,18 @@ async function renderClubs() {
                             <div class="result__info__hover__rank">Rank: ${data.rank}</div>
                         </div>`
     }).join("");
-    
-
 
     clubList.innerHTML = clubsHTML;
-    
 }
+
+const searchBarClick = async () =>{
+    const value = document.querySelector(".nav__input").value;
+    if(value != ""){
+        let data  = await fetchTeam(value);
+        clubHTML(data);
+    }
+}
+
 
 renderClubs();
 
